502aa2b3f853c06296e50b76d6a4bd337153ae25
Caprice943 <heredyamouiri@gmail.com>
2025-08-18 01:15:48 +0200

    navigation fixe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { NavbarComponent } from "./navbar/navbar.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileComponent, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
+import { ContactComponent } from './contact/contact.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { ProjectComponent } from './project/project.component';
+import { ProfileComponent } from './profile/profile.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'accueil',
+        pathMatch: 'full'
+    }
+    ,{
+        path: 'accueil',
+        component: ProfileComponent
+    },
+    {
+        path: 'experiences',
+        component: ExperiencesComponent
+    },
+    {
+        path: 'projets',
+        component: ProjectComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'accueil'
+    }
+];
