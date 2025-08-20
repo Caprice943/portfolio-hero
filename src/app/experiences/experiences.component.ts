6b2989e91c45ca5005714c75d71f26c42e519aad
Caprice943 <heredyamouiri@gmail.com>
2025-08-21 00:51:51 +0200

    squelette xp

--- src/app/experiences/experiences.component.ts
+++ src/app/experiences/experiences.component.ts
@@ -1,12 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+type Experience = {
+  period: string;
+  title: string;
+  company?: string;
+  seniority?: string;
+  description: string;
+  tags: string[];
+  link?: string;
+};
 
 @Component({
   selector: 'app-experiences',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experiences.component.html',
   styleUrl: './experiences.component.scss'
 })
+
 export class ExperiencesComponent {
+
+  experiences :  Experience [] = [
+    {
+      period: "2O22 - 2024",
+      title: " ",
+      company: " ",
+      seniority: " ",
+      description: " ",
+      tags: [ ],
+      link: " "
+    },
+    {
+      period: "2021 - 2022 ",
+      title: " ",
+      company: " ",
+      seniority: " ",
+      description: " ",
+      tags: [ ],
+      link: " "
+    }, 
+  ]
+
+  
 
 }
