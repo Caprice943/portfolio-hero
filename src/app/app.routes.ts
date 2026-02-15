38e7f96df7c95f9680beb6b43681fc9efac40221
Caprice943 <heredyamouiri@gmail.com>
2026-02-15 04:40:04 +0100

    refacto CSS home & navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
-import { ContactComponent } from './contact/contact.component';
-import { ExperiencesComponent } from './experiences/experiences.component';
-import { ProjectComponent } from './project/project.component';
 import { ProfileComponent } from './profile/profile.component';
 import { Routes } from '@angular/router';
 
